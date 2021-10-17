8377b733100e98814bd86ea362376dea1efe0fe1
Fabian Köhler <fabian.koehler@protonmail.ch>
2021-10-17 12:22:13 +0200

    Add envrc and implement I/O.

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 const { Command, Option } = require("commander");
 
-const program = new Command("render");
+
+const program = new Command("code2html");
 program.description("Renders source code files to HTML.")
 
 program
-    .addOption(new Option("-b, --backend", "rendering backend to use")
+    .addOption(new Option("-b, --backend", "Rendering backend to use")
         .choices(["prism.js", "highlight.js", "pygments"])
         .default("highlight.js"))
-    .addOption(new Option("-i, --input", "input file, \"-\" for STDIN")
+    .addOption(new Option("-i, --input", "Input file, \"-\" for STDIN")
         .default("-"))
-    .addOption(new Option("-o, --output", "output file, \"-\" for STDOUT")
+    .addOption(new Option("-o, --output", "Output file, \"-\" for STDOUT")
         .default("-"))
-    .addOption(new Option("-l, --language", "programming language used"));
+    .addOption(new Option("-l, --language", "Programming language used"));
 
 program.parse(process.argv);
 
-function readSourceCode() { }
-function writeRenderedHTML() { }
-function renderCode() { }
-function renderCodeHighlightJS() {
-    const hljs = require("highlight.js/lib/core");
-    hljs.registerLanguage("python", require("highlight.js/lib/languages/python"));
-    const renderedHTML = hljs.highlight("print('Hello World!')", { language: "python" }).value;
-    console.log(renderedHTML);
+
+function readSourceCode(input) {
+    if (input == "-") {
+        return readFileSync(process.stdin.fd, "utf-8").toString();
+    } else {
+        return readFileSync(input, "utf-8").toString();
+    }
 }
+
+function writeRenderedHTML(output, renderedCode) {
+    if (output == "-") {
+        writeFileSync(process.stdout.fd, renderedCode, "utf-8");
+    } else {
+        writeFileSync(output, renderedCode, "utf-8");
+    }
+}
+
+function renderCodeHighlightJS(sourceCode, language) {
+    const hljs = require("highlight.js/lib/core");
+    hljs.registerLanguage(language, require(`highlight.js/lib/languages/${language}`));
+    return hljs.highlight(sourceCode, { language: language }).value;
+}
+
 function renderCodePrismJS() { }
 
-renderCodeHighlightJS();
+const options = program.opts();
+console.log(options);
