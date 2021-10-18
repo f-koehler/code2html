367512b7de3730f73a751d66d44d80f699860e1d
Fabian Köhler <fabian.koehler@protonmail.ch>
2021-10-18 14:45:06 +0200

    Fix empty lines in code block.

--- index.js
+++ index.js
@@ -7,70 +7,70 @@ const program = new Command("code2html");
 program.description("Renders source code files to HTML.")
 
 program
-    .addOption(new Option("-b, --backend <backend>", "Rendering backend to use")
-        .choices(["prism.js", "highlight.js", "pygments"])
-        .default("highlight.js"))
-    .addOption(new Option("-i, --input <input>", "Input file, \"-\" for STDIN")
-        .default("-"))
-    .addOption(new Option("-o, --output <output>", "Output file, \"-\" for STDOUT")
-        .default("-"))
-    .addOption(new Option("-l, --language <language>", "Programming language used"));
+  .addOption(new Option("-b, --backend <backend>", "Rendering backend to use")
+    .choices(["prism.js", "highlight.js", "pygments"])
+    .default("highlight.js"))
+  .addOption(new Option("-i, --input <input>", "Input file, \"-\" for STDIN")
+    .default("-"))
+  .addOption(new Option("-o, --output <output>", "Output file, \"-\" for STDOUT")
+    .default("-"))
+  .addOption(new Option("-l, --language <language>", "Programming language used"));
 
 program.parse(process.argv);
 
 
 function readSourceCode(input) {
-    if (input == "-") {
-        return readFileSync(process.stdin.fd, "utf-8").toString();
-    } else {
-        return readFileSync(input, "utf-8").toString();
-    }
+  if (input == "-") {
+    return readFileSync(process.stdin.fd, "utf-8").toString();
+  } else {
+    return readFileSync(input, "utf-8").toString();
+  }
 }
 
 function writeRenderedHTML(output, renderedCode) {
-    if (output == "-") {
-        writeFileSync(process.stdout.fd, renderedCode, "utf-8");
-    } else {
-        writeFileSync(output, renderedCode, "utf-8");
-    }
+  if (output == "-") {
+    writeFileSync(process.stdout.fd, renderedCode, "utf-8");
+  } else {
+    writeFileSync(output, renderedCode, "utf-8");
+  }
 }
 
 function renderCodeHighlightJS(sourceCode, language) {
-    const hljs = require("highlight.js/lib/core");
-    hljs.registerLanguage(language, require(`highlight.js/lib/languages/${language}`));
-    const rendered = hljs.highlight(sourceCode, { language: language }).value;
-    return `<pre>\n  <code>\n${rendered}\n  </code>\n</pre>`
+  const hljs = require("highlight.js/lib/core");
+  hljs.registerLanguage(language, require(`highlight.js/lib/languages/${language}`));
+  const rendered = hljs.highlight(sourceCode, { language: language }).value;
+  return `<pre><code>${rendered}</code></pre>`
 }
 
 function renderCodePrismJS(sourceCode, language) {
-    if (language) {
-        return sourceCode;
-    }
-
-    // const prism = require("prismjs");
-    // const loadLanguages = require("prismjs/components");
-    // loadLanguages([language]);
-    // prism.highlight(sourceCode, )
-    // TODO: actual rendering
+  if (language) {
     return sourceCode;
+  }
+
+  // const prism = require("prismjs");
+  // const loadLanguages = require("prismjs/components");
+  // loadLanguages([language]);
+  // prism.highlight(sourceCode, )
+  // TODO: actual rendering
+  return sourceCode;
 }
 
 const options = program.opts();
 
 switch (options.backend) {
-    case "highlight.js": {
-        const sourceCode = readSourceCode(options.input);
-        const renderedCode = renderCodeHighlightJS(sourceCode, options.language);
-        writeRenderedHTML(options.output, renderedCode);
-        break;
-    }
-    case "prism.js": {
-        const sourceCode = readSourceCode(options.input);
-        const renderedCode = renderCodePrismJS(sourceCode, options.language);
-        writeRenderedHTML(options.output, renderedCode);
-        break;
-    }
-    default: {
-        break;
-    }
+  case "highlight.js": {
+    const sourceCode = readSourceCode(options.input);
+    const renderedCode = renderCodeHighlightJS(sourceCode, options.language);
+    writeRenderedHTML(options.output, renderedCode);
+    break;
+  }
+  case "prism.js": {
+    const sourceCode = readSourceCode(options.input);
+    const renderedCode = renderCodePrismJS(sourceCode, options.language);
+    writeRenderedHTML(options.output, renderedCode);
+    break;
+  }
+  default: {
+    break;
+  }
 }
